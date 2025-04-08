e6d7417497a91421caa00fa00390706793e91a9c
CarlosOrozcoK <corozco-2019392@kinal.edu.gt>
2025-04-08 11:52:05 -0600

    NavBar, login y register realizados

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -66,7 +66,7 @@ export const Login = ({ switchAuthHandler }) => {
 
     return (
         <div className="login-container">
-            <Logo text={'Login Kinal Cast'} />
+            <Logo text={'Login'} />
             <form className="auth-form">
                 <Input
                     field='email'
--- src/shared/hooks/index.js
+++ src/shared/hooks/index.js
@@ -1,1 +1,4 @@
 export { useLogin } from './useLogin';
+export { useRegister } from './useRegister'
+export { logout } from './userLogout.jsx'
+export { useUserDetails } from './useUserDetails.jsx'
