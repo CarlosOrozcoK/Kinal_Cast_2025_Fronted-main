de73862bfa42e94445869f2f1792e2ee48831af5
CarlosOrozcoK <corozco-2019392@kinal.edu.gt>
2025-04-22 07:22:29 -0600

    Canales terminados y errores arreglados

--- src/shared/hooks/index.js
+++ src/shared/hooks/index.js
@@ -2,3 +2,5 @@ export { useLogin } from './useLogin';
 export { useRegister } from './useRegister'
 export { logout } from './userLogout.jsx'
 export { useUserDetails } from './useUserDetails.jsx'
+export { useChannelSettings } from './useChangeSettings'
+export { useChangePassword } from './useChangePassword'
